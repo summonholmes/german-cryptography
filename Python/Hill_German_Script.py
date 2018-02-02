ba9ed4b3b80c05f1abbdfdf8f10a9c7d81fb8b1e
summonholmes <shanekimble12@hotmail.com>
2018-02-02 02:24:43 -0500

    Convert Hill_German_Script.py to use list comprehensions

--- Python/Hill_German_Script.py
+++ Python/Hill_German_Script.py
@@ -14,29 +14,18 @@ f = open("nachtlied.txt", 'r', 'iso-8859-1')
 x = f.read()
 f.close()
 
-
-dectext_ascii = []
 plaintext = PreProcess_German(x)
 plaintext_ascii_pre = [ord(c) for c in plaintext]
-
-plaintext_ascii = []
-for i in range(len(plaintext_ascii_pre)):
-    plaintext_ascii.append(plaintext_ascii_pre[i] - 97)
+plaintext_ascii = [(i - 97) for i in plaintext_ascii_pre]
 
 [K, K_1, i] = Generate(8, 30)
 ciphertext_ascii_30_pre = Hill_German(plaintext_ascii, K, 30)
-
-ciphertext_ascii_30 = []
-for i in range(len(ciphertext_ascii_30_pre)):
-    ciphertext_ascii_30.append(ciphertext_ascii_30_pre + 65)
+ciphertext_ascii_30 = [(i + 65) for i in ciphertext_ascii_30_pre]
 ciphertext_ascii = ASCII_Enc_German(ciphertext_ascii_30)
-
 ciphertext_ascii_30 = ASCII_Dec_German(ciphertext_ascii_30)
-dectext_ascii_30_pre = Hill_German(ciphertext_ascii, K_1, 30)
 
-dectext_ascii_30 = []
-for i in range(len(dectext_ascii_30_pre)):
-    dectext_ascii_30.append(dectext_ascii_30_pre[i] + 97)
+dectext_ascii_30_pre = Hill_German(ciphertext_ascii, K_1, 30)
+dectext_ascii_30 = [(i + 97) for i in dectext_ascii_30_pre]
 dectext_ascii = ASCII_30_Del(dectext_ascii_30)
 
 ciphertext = ''.join(chr(i) for i in ciphertext_ascii)
