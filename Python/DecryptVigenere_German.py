47f602d9f4ef7146542b1ae3dc40c7cc75b7ae60
summonholmes <shanekimble12@hotmail.com>
2018-02-05 01:14:58 -0500

    Vigenere routines more pythonic

--- Python/DecryptVigenere_German.py
+++ Python/DecryptVigenere_German.py
@@ -1,11 +1,6 @@
 def DecryptVigenere_German(x, k):
-    y = []
-    plaintext_ascii = [ord(c) for c in k]
-    k1 = [(i - 97) for i in plaintext_ascii]
-
-    for i in range(len(x)):
-        xp = x[i] - 65
-        ii = ((i - 1) % len(k))
-        y.append(((xp - k1[ii]) % 30) + 97)
+    key_ascii = [ord(c) for c in k]
+    k1 = [(i - 97) for i in key_ascii]
+    y = [((((x[i] - 65) - k1[(i - 1) % len(k)]) % 30) + 97) for i in range(len(x))]
 
     return y
--- Python/EncryptVigenere_German.py
+++ Python/EncryptVigenere_German.py
@@ -1,11 +1,6 @@
 def EncryptVigenere_German(x, k):
-    y = []
-    plaintext_ascii = [ord(c) for c in k]
-    k1 = [(i - 97) for i in plaintext_ascii]
-    
-    for i in range(len(x)):
-        xp = x[i] - 97
-        ii = ((i - 1) % len(k))
-        y.append(((xp + k1[ii]) % 30) + 65)
+    key_ascii = [ord(c) for c in k]
+    k1 = [(i - 97) for i in key_ascii]
+    y = [((((x[i] - 97) + k1[(i - 1) % len(k)]) % 30) + 65) for i in range(len(x))]
 
     return y
