9d7e952bdefbc6a51ad8634697f63d85e730593a
summonholmes <shanekimble12@hotmail.com>
2018-02-06 15:22:07 -0500

    Begin conversion to C++ for this project

---
+++ C++/Shift_German.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "PreProcess_German.cpp"
+
+using namespace std;
+
+void PreProcess_German(string *);
+
+int main()
+{
+    ifstream get_file("nachtlied-utf-8.txt");
+
+    if (!get_file)
+    {
+        cout << "Couldn't open the file";
+    }
+    else
+    {
+        stringstream f;
+        string x;
+
+        f << get_file.rdbuf();
+        get_file.close();
+        x = f.str();
+
+        PreProcess_German(&x);
+        cout << x;
+    }
+    return 0;
+}
