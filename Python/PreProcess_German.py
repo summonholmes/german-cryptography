71ea1237330160238a885623936af7866adea1af
Shane Kimble <shanekimble12@Hotmail.com>
2018-03-02 02:03:00 +0000

    Update PreProcess_German.py
    Convert to Pythonic one-liner

--- Python/PreProcess_German.py
+++ Python/PreProcess_German.py
@@ -3,8 +3,6 @@ from re import sub
 
 
 def PreProcess_German(x):
-    y = ''.join(x.split())
-    y = sub(r'[^\w\s]', '', y)
-    y = y.lower()
+    y = sub(r'[^\w\s]', '', (''.join(x.split()))).lower()
 
     return y
