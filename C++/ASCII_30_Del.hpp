06c5609fe6e60c9d2456732f4c88121421d66f89
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-03-14 12:29:55 -0400

    Function naming scheme corrected

--- C++/ASCII_30_Del.hpp
+++ C++/ASCII_30_Del.hpp
@@ -1,35 +1,35 @@
-void ASCII_30_Del(int *ciphertext_ascii_dec, int *array_len, int *dectext_ascii)
+void ASCII_30_Del(int *ciphertext_ascii, int *array_len, int *dectext_ascii)
 {
     int j = 0;
     for (int i = 0; i < *array_len; i++)
     {
-        if (ciphertext_ascii_dec[i] == 123)
+        if (ciphertext_ascii[i] == 123)
         {
             dectext_ascii[j] = -61;
             dectext_ascii[j + 1] = -92;
             j += 2;
         }
-        else if (ciphertext_ascii_dec[i] == 124)
+        else if (ciphertext_ascii[i] == 124)
         {
             dectext_ascii[j] = -61;
             dectext_ascii[j + 1] = -74;
             j += 2;
         }
-        else if (ciphertext_ascii_dec[i] == 125)
+        else if (ciphertext_ascii[i] == 125)
         {
             dectext_ascii[j] = -61;
             dectext_ascii[j + 1] = -68;
             j += 2;
         }
-        else if (ciphertext_ascii_dec[i] == 126)
+        else if (ciphertext_ascii[i] == 126)
         {
             dectext_ascii[j] = -61;
             dectext_ascii[j + 1] = -97;
             j += 2;
         }
-        else if (ciphertext_ascii_dec[i] > 0)
+        else if (ciphertext_ascii[i] > 0)
         {
-            dectext_ascii[j] = ciphertext_ascii_dec[i];
+            dectext_ascii[j] = ciphertext_ascii[i];
             j++;
         }
     }
