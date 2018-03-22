71edd136eb97bf1103179a6f53e46dabc7f49726
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-03-22 14:24:21 -0400

    Cleanup

--- Python/ModMatInv.py
+++ Python/ModMatInv.py
@@ -11,28 +11,25 @@ def ModMatInv(key, alpha_len):
         return inverted_key
     else:
         for i in range(len(key)):
-            key_identity_iter = key_identity_matrix[i][i]
-            ext_eucl_determinant = ExtendedEuclidean(alpha_len, key_identity_iter)
+            ext_eucl_iter = ExtendedEuclidean(alpha_len, key_identity_matrix[i][i])
             ii = i
             
-            while ext_eucl_determinant[0] != 1 and ii < (len(key) - 1):
+            while ext_eucl_iter[0] != 1 and ii < (len(key) - 1):
                 ii += 1
-                key_identity_iter = ((key_identity_matrix[ii][i] + key_identity_matrix[i][i]) % alpha_len)
-                ext_eucl_determinant = ExtendedEuclidean(alpha_len, key_identity_iter)
+                ext_eucl_iter = ExtendedEuclidean(alpha_len, ((key_identity_matrix[ii][i] + key_identity_matrix[i][i]) % alpha_len))
                 
-            if ext_eucl_determinant[0] != 1:
+            if ext_eucl_iter[0] != 1:
                 inverted_key = zeros((len(key), len(key)))
                 return inverted_key
             elif ii != i:
                 key_identity_matrix[i, :] = ((key_identity_matrix[i, :] + key_identity_matrix[ii, :]) % alpha_len)
                 
-            key_identity_matrix[i, :] = ((ext_eucl_determinant[2] * key_identity_matrix[i, :]) % alpha_len)
+            key_identity_matrix[i, :] = ((ext_eucl_iter[2] * key_identity_matrix[i, :]) % alpha_len)
             
             for j in range(len(key)):
                 if j != i:
-                    key_identity_iter = key_identity_matrix[j][i]
-                    key_identity_matrix[j, :] = ((key_identity_matrix[j, :] - key_identity_iter * key_identity_matrix[i, :]) % alpha_len)
+                    key_identity_matrix[j, :] = ((key_identity_matrix[j, :] - key_identity_matrix[j][i] * key_identity_matrix[i, :]) % alpha_len)
 
         inverted_key = key_identity_matrix[:, len(key): (2 * len(key))]
-        
+
         return inverted_key
