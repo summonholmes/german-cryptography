5b5fac5055cc57086a90b9944fa5de727c36edc7
Shane Kimble <shanekimble12@Hotmail.com>
2018-04-02 11:13:44 -0400

    Shorten

--- Python/ModMatExt.py
+++ Python/ModMatExt.py
@@ -4,8 +4,7 @@ from numpy import zeros
 
 def ModMatExt(key_identity_matrix, alpha_len, key, i, ext_eucl_i, ext_eucl):
     if ext_eucl[0] != 1:
-        key_invalid = zeros((len(key), len(key)))
-        return key_invalid
+        return zeros((len(key), len(key)))
     elif ext_eucl_i != i:
         key_identity_matrix[i, :] = (
             (key_identity_matrix[i, :] + key_identity_matrix[ext_eucl_i, :]) % alpha_len)
