49bbb75667a6c473c0e5beb5b1c8f85b71116198
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-05-14 11:47:02 -0400

    Shift Cipher Complete

--- Julia/DecryptShift_German.jl
+++ Julia/DecryptShift_German.jl
@@ -1,3 +1,3 @@
 function DecryptShift_German(ciphertext_ascii_30, magnitude_of_shift)
-    return ((ciphertext_ascii_30 - 65 - magnitude_of_shift) % 30) + 97
+    return mod((ciphertext_ascii_30 - 65 - magnitude_of_shift), 30) + 97
 end
--- Julia/EncryptShift_German.jl
+++ Julia/EncryptShift_German.jl
@@ -1,3 +1,3 @@
 function EncryptShift_German(plaintext_ascii_30, magnitude_of_shift)
-    return ((plaintext_ascii_30 - 97 + magnitude_of_shift) % 30) + 65
+    return mod((plaintext_ascii_30 - 97 + magnitude_of_shift), 30) + 65
 end
