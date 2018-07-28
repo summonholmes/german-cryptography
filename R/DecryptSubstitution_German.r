2cc0aa89cd8132cdfba37a432bf8e11049f84a6c
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-07-27 22:21:04 -0400

    Update with current knowledge

--- R/DecryptSubstitution_German.r
+++ R/DecryptSubstitution_German.r
@@ -1,24 +1,12 @@
-DecryptSubstitution_German <- function(ciphertext_ascii_dec, key_list)
-{
-  k_temp1 <- key_list
-  k_temp2 <- key_list
-  dectext_ascii_30 <- ciphertext_ascii_dec
-  for(i in 1 : length(key_list))
+DecryptSubstitution_German <-
+  function(ciphertext_ascii_dec, key_list)
   {
-    k_temp1[i] <- key_list[i] - 96
-    k_temp2[i] <- i + 96
+    k1 <- key_list - 96
+    k2 <- seq(1, length(key_list)) + 96
+    dectext_ascii_30 <- ciphertext_ascii_dec - 64
+    
+    for (i in 1:length(key_list))
+      dectext_ascii_30[dectext_ascii_30 == k1[i]] = k2[i]
+    
+    return(dectext_ascii_30)
   }
-  
-  for(i in 1 : length(ciphertext_ascii_dec))
-  {
-    for(j in 1 : length(key_list))
-    {
-      if((ciphertext_ascii_dec[i] - 64) == k_temp1[j])
-      {
-        dectext_ascii_30[i] <- k_temp2[j]
-      }
-    }
-  }
-
-  return(dectext_ascii_30)
-}
