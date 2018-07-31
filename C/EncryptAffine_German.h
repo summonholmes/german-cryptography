1b2bb8e25e4dfc871609d3566a9951da06caad36
Shane Kimble <shanekimble@Shanes-MacBook-Pro.local>
2018-07-31 09:15:05 -0400

    Remove old C code

--- C/EncryptAffine_German.h
+++
@@ -1,7 +0,0 @@
-void EncryptAffine_German(int *plaintext_ascii_30, int *a, int *b, int *alpha_len, int *array_len, int *ciphertext_acsii_30)
-{
-    for (int i = 0; i < *array_len; i++)
-    {
-        ciphertext_acsii_30[i] = ((*a * (plaintext_ascii_30[i] - 97) + *b) % *alpha_len) + 65;
-    }
-}
--- C/EncryptSubstitution_German.h
+++
@@ -1,7 +0,0 @@
-void EncryptSubstitution_German(int *plaintext_ascii_30, int *array_len, int *key_list, int *ciphertext_ascii_30)
-{
-    for (int i = 0; i < *array_len; i++)
-    {
-        ciphertext_ascii_30[i] = key_list[plaintext_ascii_30[i] - 97] - 32;
-    }
-}
