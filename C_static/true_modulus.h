f99a5a40216c7cdd26a797d8fde2f0db8fd7776a
Shane Kimble <skimble@edjanalytics.com>
2019-01-02 15:40:06 -0500

    Clean and update C

--- C_static/true_modulus.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRUE_MODULUS_H
-#define TRUE_MODULUS_H
-
-int true_modulus(int r, int q)
-{ /* Modulus in C with negatives doesn't work */
-    return (r % q + q) % q;
-}
-
-#endif /* TRUE_MODULUS_H */
