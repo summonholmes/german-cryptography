1a4e512196d17e81e9307af35d32f89f80f1cfdb
Shane Kimble <skimble@edjanalytics.com>
2019-01-03 10:37:39 -0500

    Add semicolon, change comments

--- JavaScript/EncryptAffine_German.js
+++ JavaScript/EncryptAffine_German.js
@@ -1,7 +1,7 @@
 import { mod } from "./mod.js";
 
 export function EncryptAffine_German(num) {
-    // Shift using y = ax + b
+    // Scramble using y = ax + b
     var a = 7;
     var b = 5;
     return mod(a * (num - 97) + b, 30) + 65;
